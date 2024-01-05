275d5d5e6a5b9cfcae002c14fdb10fa0fcc97d04
i. Bernal <33322694+jbernalr@users.noreply.github.com>
2024-01-05 13:48:59 +0100

    more details and slower

--- starwars.c
+++ starwars.c
@@ -1,53 +1,53 @@
-static const note_t starWarsTheme[] = { 
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_F4, QUARTER },
-  { PITCH_C5, EIGHTH },
-  { PITCH_A4, EIGHTH },
+  { PITCH_A4, QUARTER },      // Note 1
+  { PITCH_A4, EIGHTH },       // Note 2
+  { PITCH_A4, EIGHTH },       // Note 3
+  { PITCH_F4, HALF },         // Note 4
+  { PITCH_C5, EIGHTH },       // Note 5
+  { PITCH_A4, EIGHTH },       // Note 6
 
-  { PITCH_G4, HALF },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, HALF },
+  { PITCH_G4, HALF },         // Note 7
+  { PITCH_E4, QUARTER },      // Note 8
+  { PITCH_E4, EIGHTH },       // Note 9
+  { PITCH_E4, EIGHTH },       // Note 10
+  { PITCH_E4, QUARTER },      // Note 11
 
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_F4, QUARTER },
-  { PITCH_C5, EIGHTH },
-  { PITCH_A4, EIGHTH },
+  { PITCH_A4, EIGHTH },       // Note 12
+  { PITCH_A4, EIGHTH },       // Note 13
+  { PITCH_A4, EIGHTH },       // Note 14
+  { PITCH_F4, HALF },         // Note 15
+  { PITCH_C5, EIGHTH },       // Note 16
+  { PITCH_A4, EIGHTH },       // Note 17
 
-  { PITCH_G4, HALF },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, HALF },
+  { PITCH_G4, HALF },         // Note 18
+  { PITCH_E4, EIGHTH },       // Note 19
+  { PITCH_E4, EIGHTH },       // Note 20
+  { PITCH_E4, QUARTER },      // Note 21
 
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_F4, QUARTER },
-  { PITCH_C5, EIGHTH },
-  { PITCH_A4, EIGHTH },
+  { PITCH_A4, EIGHTH },       // Note 22
+  { PITCH_A4, EIGHTH },       // Note 23
+  { PITCH_A4, EIGHTH },       // Note 24
+  { PITCH_F4, HALF },         // Note 25
+  { PITCH_C5, EIGHTH },       // Note 26
+  { PITCH_A4, EIGHTH },       // Note 27
 
-  { PITCH_G4, HALF },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, EIGHTH },
-  { PITCH_F4, EIGHTH },
-  { PITCH_G4, QUARTER },
+  { PITCH_G4, HALF },         // Note 28
+  { PITCH_E4, EIGHTH },       // Note 29
+  { PITCH_E4, EIGHTH },       // Note 30
+  { PITCH_E4, EIGHTH },       // Note 31
+  { PITCH_E4, EIGHTH },       // Note 32
+  { PITCH_F4, QUARTER },      // Note 33
+  { PITCH_G4, QUARTER },      // Note 34
 
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_A4, EIGHTH },
-  { PITCH_F4, QUARTER },
-  { PITCH_C5, EIGHTH },
-  { PITCH_A4, EIGHTH },
+  { PITCH_A4, EIGHTH },       // Note 35
+  { PITCH_A4, EIGHTH },       // Note 36
+  { PITCH_A4, EIGHTH },       // Note 37
+  { PITCH_F4, HALF },         // Note 38
+  { PITCH_C5, EIGHTH },       // Note 39
+  { PITCH_A4, EIGHTH },       // Note 40
 
-  { PITCH_G4, HALF },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, EIGHTH },
-  { PITCH_E4, HALF },
+  { PITCH_G4, HALF },         // Note 41
+  { PITCH_E4, EIGHTH },       // Note 42
+  { PITCH_E4, EIGHTH },       // Note 43
+  { PITCH_E4, QUARTER },      // Note 44
 
-  { PITCH_END, 0 }
-};
+  { PITCH_END, 0 }            // End of the array
